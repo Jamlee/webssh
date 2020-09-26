5aa0968b6c748d3465dedbab342790d1860855b8
jamlee <jamlee@jamlee.cn>
2020-09-26 16:49:35 +0800

    添加 k8s exec 代理

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # WEBSSH
 
-88 行代码完成一个简易的webssh，用于理解原理。
+88 行代码完成一个简易的webssh，用于理解实现webssh原理。我们也可以基于该原理开发 Paas 平台功能。
 
 ## 使用方式
+
+针对于虚拟机的 webssh
+
+```bash
+# 1 启动 node
+node main.js
+
+# 打开本地文件 index-vm.html
+# 修改代码里配置信息
+# https://github.com/Jamlee/webssh/blob/033491e4f17fc40794e84e369550d9f20f290af0/index.html#L11
+
+let config = {msgId: "server01", ip: "200.200.200.180", username: "root", password: "YOUR PASSWORD"};
+# 然后本地浏览器打开 index-vm.html
+``` 
+
+针对于k8s 的 webssh
 
 ```bash
 # 1 启动 node
@@ -12,10 +28,14 @@ node main.js
 # 修改代码里配置信息
 # https://github.com/Jamlee/webssh/blob/033491e4f17fc40794e84e369550d9f20f290af0/index.html#L11
 
-let config = {msgId: "server01", ip: "200.200.200.180", username: "root", password: "YOUR PASSWORD"};
-# 然后本地浏览器打开
+let config = {msgId: "pod01", podName: "tiller-deploy-6d8dfbb696-z8g6l", namespace: "kube-system"};
+# 然后本地浏览器打开 index-kube.html
 ``` 
 
 ## 效果图
 
+VM 的 webssh
 ![image](http://github.com/Jamlee/webssh/raw/master/01.jpg)
+
+k8s 中 pod 的 webssh
+![image](http://github.com/Jamlee/webssh/raw/master/02.jpg)
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const utf8 = require('utf8');
 const SSHClient = require('ssh2').Client;
+const pty = require('node-pty');
+const os = require('os');
 
 function createNewServer(machineConfig, socket) {
     const ssh = new SSHClient();
@@ -35,11 +37,40 @@ function createNewServer(machineConfig, socket) {
     });
 }
 
+function createNewPod(podConfig, socket) {
+    let {cols, rows, msgId, namespace, podName} = podConfig;
+    const env = Object.assign({}, process.env);
+    env['COLORTERM'] = 'truecolor';
+    term = pty.spawn('/usr/local/bin/kubectl',
+        ['exec', '-it', podName, '-n', namespace, '--', '/bin/sh'], {
+        name: 'xterm-256color',
+        cols: cols || 80,
+        rows: rows || 24,
+        cwd: env.PWD,
+        env: env,
+        encoding: 'utf8', // 为空是 binary 格式
+    });
+    term.onData((data) => {
+        socket.emit(msgId, data);
+    });
+    term.onExit( (code, signal) => {
+        socket.emit(msgId, '\r\nclose\r\n');
+        console.log(`child process exited with code ${code}`);
+    });
+    socket.on(msgId, function (data) {
+        term.write(data);
+    });
+}
 
 io.on('connection', function(socket) {
     socket.on('createNewServer', function(machineConfig) {//新建一个ssh连接
         console.log("createNewServer")
         createNewServer(machineConfig, socket);
+    })
+
+    socket.on('createNewPod', function(podConfig) {//新建一个pod ssh连接
+        console.log("createNewPod")
+        createNewPod(podConfig, socket);
     })
 
     socket.on('disconnect', function(){
